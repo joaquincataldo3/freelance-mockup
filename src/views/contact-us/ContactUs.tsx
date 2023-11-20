2580cc5c04adc0201bac00f033e9cd638c6a267f
joaquincataldo3 <joaco.cataldo3@gmail.com>
2023-11-20 18:08:35 -0300

    error hand

--- src/views/contact-us/ContactUs.tsx
+++ src/views/contact-us/ContactUs.tsx
@@ -5,13 +5,13 @@ import './ContactUs.css';
 
 export const ContactUs = () => {
 
-  const imgSrc = './src/assets/wedding.jpg';
+  
   const mapSrc = "https://www.google.com/maps/embed?pb=!1m14!1m12!1m3!1d8535.649117282257!2d-80.22003769223944!3d25.908997963846122!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!5e0!3m2!1ses!2sar!4v1699899698944!5m2!1ses!2sar";
 
 
   return (
     <>
-        <FirstViewBackImage src={imgSrc} />
+        <FirstViewBackImage  />
         <main>
             <h1 className="main-title">Contact us</h1>
             <div className="contact-us-box-container">
