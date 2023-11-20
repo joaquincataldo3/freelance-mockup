28896cd062c0be820d80805d349152a27c6fbe3d
joaquincataldo3 <joaco.cataldo3@gmail.com>
2023-11-20 18:07:28 -0300

    text box

--- src/views/home/components/carousel-title-box/CarouselTitleBox.tsx
+++ src/views/home/components/carousel-title-box/CarouselTitleBox.tsx
@@ -4,7 +4,7 @@ export const CarouselTitleBox = () => {
   return (
     <div className='carousel-box-container'>
         <h1>Dream Maker</h1>
-        <p className='carousel-box-description'>We would love to meet up and chat about how we can make your dream wedding happen !</p>
+        <p className='carousel-box-description'>We would love to meet up and chat about how we can make your dream wedding happen</p>
         <div className="shop-now-container">
             <a href='' className='shop-now-a'>
               <p>Explore</p>
