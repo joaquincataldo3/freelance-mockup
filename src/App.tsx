8b300fb5d994f6911e5caf869667babf24e50b2a
joaquincataldo3 <joaco.cataldo3@gmail.com>
2023-11-21 09:27:17 -0300

    prod not

--- src/App.tsx
+++ src/App.tsx
@@ -17,10 +17,8 @@ function App() {
       <Navbar />
       <Routes>
         <Route path="/about" element={<About/>}/>
-        <Route path="/services" element={<Services/>}>
-
-        <Route path=":serviceId" element={<ServiceDetail />} />
-        </Route>
+        <Route path="/services" element={<Services/>} />
+        <Route path="/services/:serviceId" element={<ServiceDetail />} />
         <Route path="/contact" element={<ContactUs />}/>
         <Route path="/" element={<Home/>}/>
       </Routes>
